a2114f2ed3440fc177a672e0d1a19067a53a6f54
janae-b <janae.boersma@comcast.net>
2021-05-07 11:19:17 -0500

    add a add a game link

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Game
 from django.http import HttpResponse
-
-# class Game: 
-#   def __init__(self, name, description, number):
-#     self.name = name
-#     self.description = description
-#     self.number = number
-
-# games = [
-#   Game('Pinata', 'Hit it!', 20),
-#   Game('JackBox Games', 'Virtual games you can play anywhere with anyone!', 8),
-#   Game('Unwrap the Saran Wrap', 'Find the prizes within the saran wrap!', 10)
-# ]
 
 def home(request):
   return render(request, 'home.html')
@@ -27,6 +16,10 @@ def games_index(request):
 def games_detail(request, game_id):
   game = Game.objects.get(id=game_id)
   return render(request, 'games/detail.html', { 'game': game })
+
+class GameCreate(CreateView):
+  model = Game
+  fields = '__all__'
 
 
 
