1300e3198626b040d6e4da1ec6c5aec6b4708737
janae-b <janae.boersma@comcast.net>
2021-05-07 13:38:06 -0500

    add in planning phases

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game, Planning
 
 admin.site.register(Game)
+admin.site.register(Planning)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+PHASES = (
+      ('M', 'Materials'),
+      ('T', 'Test'),
+      ('R', 'Ready to Play')
+)
+
 class Game(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
@@ -11,4 +17,15 @@ class Game(models.Model):
 
   def get_absolute_url(self):
       return reverse("detail", kwargs={"game_id": self.id})
-  
+
+class Planning(models.Model):
+  date = models.DateField('complete date')
+  phase = models.CharField(
+    max_length=1,
+    choices=PHASES,
+    default=PHASES[0][0]
+  )
+  game = models.ForeignKey(Game, on_delete=models.CASCADE)  
+
+  def __str__(self):
+    return f"{self.get_phase_display()} on {self.date}"
