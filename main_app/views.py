52b9d8da8e8a0408f1dac1630c8de51858ad0528
janae-b <janae.boersma@comcast.net>
2021-05-07 10:19:29 -0500

    fix game list

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Game: 
+  def __init__(self, name):
+    self.name = name
+
+
+games = [
+  Game('Pinata'),
+  Game('JackBox Games'),
+  Game('Unwrap the Saran Wrap')
+]
 
 def home(request):
   return HttpResponse('<h1>Welcome to the Party Game Planner</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def games_index(request):
+  return render(request, 'games/index.html', { 'games': games })
+
+
+
