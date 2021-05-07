03110eb3960f42ab725f7ee90e54e8e4d263e986
janae-b <janae.boersma@comcast.net>
2021-05-07 10:31:16 -0500

    create homepage

--- main_app/views.py
+++ main_app/views.py
@@ -14,7 +14,7 @@ games = [
 ]
 
 def home(request):
-  return HttpResponse('<h1>Welcome to the Party Game Planner</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
