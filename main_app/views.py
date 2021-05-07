051a016232b40c2369d0295633df66c5ade742f5
janae-b <janae.boersma@comcast.net>
2021-05-07 10:46:48 -0500

    add superuser

--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
+from .models import Game
 from django.http import HttpResponse
 
-class Game: 
-  def __init__(self, name, description, number):
-    self.name = name
-    self.description = description
-    self.number = number
+# class Game: 
+#   def __init__(self, name, description, number):
+#     self.name = name
+#     self.description = description
+#     self.number = number
 
-games = [
-  Game('Pinata', 'Hit it!', 20),
-  Game('JackBox Games', 'Virtual games you can play anywhere with anyone!', 8),
-  Game('Unwrap the Saran Wrap', 'Find the prizes within the saran wrap!', 10)
-]
+# games = [
+#   Game('Pinata', 'Hit it!', 20),
+#   Game('JackBox Games', 'Virtual games you can play anywhere with anyone!', 8),
+#   Game('Unwrap the Saran Wrap', 'Find the prizes within the saran wrap!', 10)
+# ]
 
 def home(request):
   return render(request, 'home.html')
@@ -20,6 +21,7 @@ def about(request):
   return render(request, 'about.html')
 
 def games_index(request):
+  games = Game.objects.all()
   return render(request, 'games/index.html', { 'games': games })
 
 
