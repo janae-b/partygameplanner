cc4173108b6711cab5229c8f1d2acea99a090656
janae-b <janae.boersma@comcast.net>
2021-05-07 23:39:32 -0500

    can add more events

--- main_app/views.py
+++ main_app/views.py
@@ -17,8 +17,9 @@ def games_index(request):
 
 def games_detail(request, game_id):
   game = Game.objects.get(id=game_id)
+  events_games_arent_played = Event.objects.exclude(id__in = game.events.all().values_list('id'))
   planning_form = PlanningForm()
-  return render(request, 'games/detail.html', { 'game': game, 'planning_form': planning_form })
+  return render(request, 'games/detail.html', { 'game': game, 'planning_form': planning_form, 'events': events_games_arent_played })
 
 class GameCreate(CreateView):
   model = Game
@@ -32,6 +33,10 @@ class GameUpdate(UpdateView):
 class GameDelete(DeleteView):
   model = Game
   success_url = '/games/'
+
+def assoc_event(request, game_id, event_id):
+  Game.objects.get(id=game_id).events.add(event_id)
+  return redirect('detail', game_id=game_id)
 
 def add_planning(request, game_id):
   # create a ModelForm instance using the data in request.POST
@@ -62,4 +67,6 @@ class EventUpdate(UpdateView):
 class EventDelete(DeleteView):
   model = Event
   success_url = '/events/'
+
+
 
