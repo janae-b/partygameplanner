e1decdc2d10f7658467669d2a924236ef6669bcc
janae-b <janae.boersma@comcast.net>
2021-05-11 22:19:34 -0500

    fix photos

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Game, Party, Plan
+from .models import Game, Party, Plan, Photo
 
 admin.site.register(Game)
 admin.site.register(Party)
 admin.site.register(Plan)
+admin.site.register(Photo)
